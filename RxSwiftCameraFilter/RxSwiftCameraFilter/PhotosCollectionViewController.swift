6e07131786edd0cdc5f660e2714e41793e7748a0
Mumani <yb2316@naver.com>
2021-03-16 22:09:49 +0900

    :construction: 사진 접근 권한 추가 및 프로젝트로 이미지 가져오기

--- RxSwiftCameraFilter/RxSwiftCameraFilter/PhotosCollectionViewController.swift
+++ RxSwiftCameraFilter/RxSwiftCameraFilter/PhotosCollectionViewController.swift
@@ -6,7 +6,27 @@
 //
 
 import UIKit
+import Photos
 
 final class PhotosCollectionViewController: UICollectionViewController {
 
+    private var images = [PHAsset]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        populatePhotos()
+    }
+
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+            if status == .authorized {
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                assets.enumerateObjects { object, count, stop in
+                    self?.images.append(object)
+                }
+                self?.images.reverse()
+            }
+        }
+    }
 }
