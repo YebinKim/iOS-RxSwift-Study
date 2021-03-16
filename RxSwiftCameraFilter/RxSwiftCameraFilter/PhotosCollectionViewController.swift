f4e212d40541ec3a8d84828df354e21fb9e81cc7
Mumani <yb2316@naver.com>
2021-03-16 21:37:14 +0900

    :tada: 프로젝트 생성 및 UI 구현

---
+++ RxSwiftCameraFilter/RxSwiftCameraFilter/PhotosCollectionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewController.swift
+//  RxSwiftCameraFilter
+//
+//  Created by Yebin Kim on 2021/03/16.
+//
+
+import UIKit
+
+final class PhotosCollectionViewController: UICollectionViewController {
+
+}
---
+++ RxSwiftCameraFilter/RxSwiftCameraFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftCameraFilter
+//
+//  Created by Yebin Kim on 2021/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
