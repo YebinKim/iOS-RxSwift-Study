995bc7b50156b731c400433c4b14b1f9b0df3179
Mumani <yb2316@naver.com>
2021-04-13 22:36:35 +0900

    :construction: AppCoordinator 생성 및 SceneDelegate에 Coordinator 적용
    - RxSwift+MVVM 샘플 프로젝트 Part. 1
    - https://www.youtube.com/watch?v=Dentp0JUbyI

---
+++ RxSwift+MVVMSample/MVVMRxSwift/MVVMRxSwift/AppCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  AppCoordinator.swift
+//  MVVMRxSwift
+//
+//  Created by Yebin Kim on 2021/04/13.
+//
+
+import UIKit
+
+class AppCoordinator {
+
+    private let window: UIWindow
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    func start() {
+        let viewController = ViewController.instantiate()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
--- RxSwift+MVVMSample/MVVMRxSwift/MVVMRxSwift/ViewController.swift
+++ RxSwift+MVVMSample/MVVMRxSwift/MVVMRxSwift/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    static func instantiate() -> ViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+        return viewController
     }
 
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
 
