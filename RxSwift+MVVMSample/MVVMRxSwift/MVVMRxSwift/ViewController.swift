6a27a77131112ee2a31b52600b972e721fff9866
Mumani <yb2316@naver.com>
2021-04-13 23:28:37 +0900

    :construction: 테이블 뷰 생성 및 RxCocoa Binding 적용
    - RxSwift+MVVM 샘플 프로젝트 Part. 3
    - https://www.youtube.com/watch?v=kraICEpu7Qg

--- RxSwift+MVVMSample/MVVMRxSwift/MVVMRxSwift/ViewController.swift
+++ RxSwift+MVVMSample/MVVMRxSwift/MVVMRxSwift/ViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     private let disposeBag = DisposeBag()
 
     private var viewModel: RestaurantsListViewModel!
+
+    @IBOutlet weak var tableView: UITableView!
 
     static func instantiate(viewModel: RestaurantsListViewModel) -> ViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
@@ -24,7 +27,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        initializeTableView()
+
         viewModel.fetchRestaurantViewModel()
+            // UI 관련 작업은 MainScheduler 에서 수행
+            .observe(on: MainScheduler.instance)
+            // tableView cell에 viewModel 데이터를 바인딩
+            .bind(to: tableView.rx.items(cellIdentifier: "cell")) { index, viewModel, cell in
+                cell.textLabel?.text = viewModel.displayTest
+            }
+            .disposed(by: disposeBag)
+    }
+
+    private func initializeTableView() {
+        tableView.tableFooterView = UIView()
+
+        navigationItem.title = viewModel.title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.contentInsetAdjustmentBehavior = .never
     }
 }
 
