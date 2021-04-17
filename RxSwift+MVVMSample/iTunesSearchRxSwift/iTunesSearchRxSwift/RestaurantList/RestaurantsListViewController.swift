148bb5de1c05f4d16f48f2f8ceb0cc79accde8e5
Mumani <yb2316@naver.com>
2021-04-17 22:23:13 +0900

    :tada: iTunes Search API + RxSwift + MVVM 프로젝트 생성

---
+++ RxSwift+MVVMSample/iTunesSearchRxSwift/iTunesSearchRxSwift/RestaurantList/RestaurantsListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RestaurantsListViewController.swift
+//  MVVMRxSwift
+//
+//  Created by Yebin Kim on 2021/04/13.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class RestaurantsListViewController: UIViewController {
+
+    private let disposeBag = DisposeBag()
+
+    private var viewModel: RestaurantsListViewModel!
+
+    @IBOutlet weak var tableView: UITableView!
+
+    static func instantiate(viewModel: RestaurantsListViewModel) -> RestaurantsListViewController {
+        let storyboard = UIStoryboard(name: "RestaurantsListView", bundle: .main)
+        let viewController = storyboard.instantiateInitialViewController() as! RestaurantsListViewController
+        viewController.viewModel = viewModel
+        return viewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initializeTableView()
+
+        viewModel.fetchRestaurantViewModel()
+            // UI 관련 작업은 MainScheduler 에서 수행
+            .observe(on: MainScheduler.instance)
+            // tableView cell에 viewModel 데이터를 바인딩
+            .bind(to: tableView.rx.items(cellIdentifier: "cell")) { index, viewModel, cell in
+                cell.textLabel?.text = viewModel.displayTest
+            }
+            .disposed(by: disposeBag)
+    }
+
+    private func initializeTableView() {
+        tableView.tableFooterView = UIView()
+
+        navigationItem.title = viewModel.title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.contentInsetAdjustmentBehavior = .never
+    }
+}
+
