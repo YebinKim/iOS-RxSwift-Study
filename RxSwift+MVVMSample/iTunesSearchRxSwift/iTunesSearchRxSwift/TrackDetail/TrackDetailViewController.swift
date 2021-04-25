11e6bbd0107229eaaba05c2b75a582ed264f8b78
Mumani <yb2316@naver.com>
2021-04-26 03:54:01 +0900

    [#5] 음악 재생 기능 구현 및 재생 버튼, 재생 슬라이더, 볼륨 슬라이더 바인딩

--- RxSwift+MVVMSample/iTunesSearchRxSwift/iTunesSearchRxSwift/TrackDetail/TrackDetailViewController.swift
+++ RxSwift+MVVMSample/iTunesSearchRxSwift/iTunesSearchRxSwift/TrackDetail/TrackDetailViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import AVFoundation
 
 final class TrackDetailViewController: UIViewController {
 
@@ -18,6 +19,7 @@ final class TrackDetailViewController: UIViewController {
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var songNameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var volumeSlider: UISlider!
     @IBOutlet weak var playSlider: UISlider!
     @IBOutlet weak var playButton: UIButton!
 
@@ -42,6 +44,43 @@ final class TrackDetailViewController: UIViewController {
                 self.thumbnailImageView.setImage(urlString: $0.thumbnailUrl)
                 self.songNameLabel.text = $0.songName
                 self.artistNameLabel.text = $0.artistName
+            }
+            .disposed(by: disposeBag)
+
+        viewModel.player
+            .asDriver()
+            .drive { player in
+                self.playButton.rx.tap
+                    .bind {
+                        if player.timeControlStatus == .paused {
+                            player.play()
+                            self.playButton.setTitle("Pause", for: .normal)
+                        } else {
+                            player.pause()
+                            self.playButton.setTitle("Play", for: .normal)
+                        }
+                    }
+                    .disposed(by: self.disposeBag)
+
+                self.volumeSlider.rx.value
+                    .bind(to: player.rx.volume)
+                    .disposed(by: self.disposeBag)
+
+                self.playSlider.rx.value
+                    .subscribe(onNext: { value in
+                        guard let item = player.currentItem else { return }
+                        let nowSeconds: Double = Double(value) * item.duration.seconds
+                        let seekTime = CMTime(seconds: nowSeconds, preferredTimescale: 100)
+                        player.currentItem?.seek(to: seekTime, completionHandler: nil)
+                    })
+                    .disposed(by: self.disposeBag)
+
+                player.addPeriodicTimeObserver(
+                    forInterval: .init(value: 1, timescale: 100),
+                    queue: .main) { _ in
+                    guard let item = player.currentItem else { return }
+                    self.playSlider.value = Float(item.currentTime().seconds / item.duration.seconds)
+                }
             }
             .disposed(by: disposeBag)
     }
--- RxSwift+MVVMSample/iTunesSearchRxSwift/iTunesSearchRxSwift/TrackDetail/TrackDetailViewModel.swift
+++ RxSwift+MVVMSample/iTunesSearchRxSwift/iTunesSearchRxSwift/TrackDetail/TrackDetailViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import RxSwift
 import RxRelay
+import AVFoundation
 
 final class TrackDetailViewModel {
 
@@ -16,8 +17,14 @@ final class TrackDetailViewModel {
     private let _track: Track
 
     lazy var selectedTrack = BehaviorRelay<Track>(value: _track)
+    let player = BehaviorRelay<AVPlayer>(value: AVPlayer())
 
     init(track: Track) {
         self._track = track
+
+        if let previewUrl = URL(string: track.previewUrl) {
+            let playerItem = AVPlayerItem(url: previewUrl)
+            player.accept(AVPlayer(playerItem: playerItem))
+        }
     }
 }
